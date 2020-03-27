a8d7d74cea4214e8ecbc012218b98a9efaa4f329
kjuulh <hermansendev@gmail.com>
2020-03-27 22:57:35 +0100

    Upgrade dependencies

--- app/src/app/modules/material/material.module.ts
+++ app/src/app/modules/material/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { MatCheckboxModule } from "@angular/material";
-import { MatButtonModule } from "@angular/material";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatDatepickerModule } from "@angular/material/datepicker";
